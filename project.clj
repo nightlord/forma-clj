770f606a700cde2e2a79db3219575658b9d12125
jiyouyou <zzh_621@yahoo.com>
2016-01-27 19:19:16 +0800

    use clojure 1.6

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :dev-resources-paths ["dev"]
   :jvm-opts ["-XX:+UseConcMarkSweepGC" "-Xms1024M" "-Xmx1048M" "-server"]
   :main nil
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.csv "0.1.2"]
@@ -18,8 +18,8 @@
                  [clj-time "0.7.0"]
                  [forma/gdal "1.8.0"]
                  [forma/jblas "1.2.1"]
-                 [cascalog "3.0.0-SNAPSHOT"]
-                 [cascalog/cascalog-checkpoint "3.0.0-SNAPSHOT"]
+                 [cascalog "2.1.1"]
+                 [cascalog/cascalog-checkpoint "2.1.1"]
                  [com.backtype/dfs-datastores "1.3.6"]
                  [com.backtype/dfs-datastores-cascading "1.3.6"]
                  [org.apache.thrift/libthrift "0.8.0"
@@ -27,7 +27,7 @@
                  [net.lingala.zip4j/zip4j "1.3.1"]]
   :aot [forma.hadoop.pail, forma.schema, #"forma.hadoop.jobs.*"]
   :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-core "1.2.1"]]}
-             :dev {:dependencies [[cascalog/midje-cascalog "3.0.0-SNAPSHOT"]
+             :dev {:dependencies [[cascalog/midje-cascalog "2.1.1"]
                                   [incanter/incanter-charts "1.3.0"]]
                    :plugins [[lein-midje "3.1.3"]
                              [lein-emr "0.2.0-SNAPSHOT"]]}})
